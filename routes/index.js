cb3ab744ec52aa9ac4ba830cc7e09eb70a43c342
Imagine-Me <prince100thomas@gmail.com>
2021-03-04 17:59:39 +0530

    Add routing for category endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+const products = require('../data/products.json')
 
-router.get('/__test', function(req, res, next) {
+
+router.get('/__test', function (req, res, next) {
   res.send("Hello World")
+});
+
+router.get('/category/:name', (req, res) => {
+  const category = req.params.name
+  const result = products.filter(product => product.category === category)
+  res.send(result);
 });
 
 module.exports = router;
