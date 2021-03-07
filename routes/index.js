60cef65f1eda7190407a35ee67b7297e7d8aaa5e
Imagine-Me <prince100thomas@gmail.com>
2021-03-07 16:17:05 +0530

    Add new data for products

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,36 @@ router.get('/__test', function (req, res, next) {
   res.send("Hello World")
 });
 
-router.get('/category/:name', (req, res) => {
+router.post('/category/:name', (req, res) => {
+  const filterCriteria = req.body
   const category = req.params.name
-  const result = products.filter(product => product.category === category)
-  res.send(result);
+  const response = {}
+  let data = products.filter(product => product.category === category)
+  console.log(filterCriteria);
+  if (filterCriteria.isFilter) {
+    data = data.filter(product => {
+      return (filterCriteria.price.length < 2 || (product.currentPrice >= filterCriteria.price[0] && product.currentPrice <= filterCriteria.price[1])) &&
+        (filterCriteria.rating.length === 0 || (filterCriteria.rating.includes('' + product.rating))) &&
+        (filterCriteria.brands.length === 0 || (filterCriteria.brands.includes(product.brand)))
+    })
+    response.data = data
+    response.filter = null
+  } else {
+    const filter = {}
+    const prices = data.map(product => product.currentPrice)
+    const brands = []
+    data.forEach(product => {
+      if (!brands.includes(product.brand)) {
+        brands.push(product.brand)
+      }
+    })
+    filter.brands = brands
+    filter.price = [Math.min(...prices), Math.max(...prices)]
+    response.filter = filter
+    response.data = data
+  }
+  res.send(response);
 });
+
 
 module.exports = router;
